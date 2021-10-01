c9461e0008fcd5b89a414980a435e9f8e8ac6539
gosoccer <82986227+gosoccer@users.noreply.github.com>
2021-10-01 11:22:34 +0900

    Update 8.lb_probe.tf

--- 8.lb_probe.tf
+++ 8.lb_probe.tf
@@ -1,6 +1,6 @@
-resource "azurerm_lb_probe" "david-lb-probe" {
-    resource_group_name = azurerm_resource_group.david-rg.name
-    loadbalancer_id = azurerm_lb.david-lb.id
+resource "azurerm_lb_probe" "user29-lb-probe" {
+    resource_group_name = azurerm_resource_group.user29-rg.name
+    loadbalancer_id = azurerm_lb.user29-lb.id
     name = "http-probe"
     protocol = "Http"
     request_path = "/"
