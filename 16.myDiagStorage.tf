f88d56a5db3ec5e6a7efc6714f833a3d62893f72
gosoccer <82986227+gosoccer@users.noreply.github.com>
2021-10-01 11:37:20 +0900

    Update 16.myDiagStorage.tf

--- 16.myDiagStorage.tf
+++ 16.myDiagStorage.tf
@@ -1,7 +1,7 @@
 resource "azurerm_storage_account" "mystorageaccount" {
-    name                = "davidvm1diagestorage1"
-    resource_group_name = azurerm_resource_group.david-rg.name
-    location = azurerm_resource_group.david-rg.location
+    name                = "user29vm1diagestorage1"
+    resource_group_name = azurerm_resource_group.user29-rg.name
+    location = azurerm_resource_group.user29-rg.location
     account_replication_type = "LRS"
     account_tier = "Standard"
 
