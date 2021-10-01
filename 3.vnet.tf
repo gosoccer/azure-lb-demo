e41b0776ba299c83fe4e882771161e4443057f08
gosoccer <82986227+gosoccer@users.noreply.github.com>
2021-10-01 11:16:55 +0900

    Update 3.vnet.tf

--- 3.vnet.tf
+++ 3.vnet.tf
@@ -1,6 +1,6 @@
-resource "azurerm_virtual_network" "david-net" {
-    name = "david-vnet"
-    address_space = ["40.0.0.0/16"]
-    location = azurerm_resource_group.david-rg.location
-    resource_group_name = azurerm_resource_group.david-rg.name
+resource "azurerm_virtual_network" "user29-net" {
+    name = "user29-vnet"
+    address_space = ["29.0.0.0/16"]
+    location = azurerm_resource_group.user29-rg.location
+    resource_group_name = azurerm_resource_group.user29-rg.name
 }
